7c68792aceee4ae4200076724546e5bb5194279b
peknight <JKpeknight@gmail.com>
2025-03-26 08:22:03 +0800

    update

--- http-core/shared/src/main/scala/com/peknight/http/media/MediaRange.scala
+++
@@ -1,7 +0,0 @@
-package com.peknight.http.media
-
-import org.http4s.MediaType
-
-object MediaRange:
-  val `application/problem+json`: MediaType = MediaType.unsafeParse("application/problem+json")
-end MediaRange
