31ffd6e099bfdec44bf2863d3840962713450129
peknight <JKpeknight@gmail.com>
2026-02-03 17:27:57 +0800

    先写.part再重命名

--- http-client/jvm/src/main/scala/com/peknight/http/client/download/package.scala
+++ http-client/jvm/src/main/scala/com/peknight/http/client/download/package.scala
@@ -3,6 +3,7 @@ package com.peknight.http.client
 import cats.Monad
 import cats.data.EitherT
 import cats.effect.Async
+import cats.syntax.applicative.*
 import com.peknight.cats.syntax.eitherT.{eLiftET, rLiftET}
 import com.peknight.error.Error
 import com.peknight.error.option.OptionEmpty
@@ -14,33 +15,25 @@ import org.http4s.client.Client
 import org.http4s.{Request, Response}
 
 package object download:
-  def download[F[_]](request: Request[F], directory: Option[Path] = None, fileName: Option[Path] = None, maxRedirects: Int = 5)
+  def download[F[_]](request: Request[F], directory: Option[Path] = None, fileName: Option[Path] = None,
+                     overwrite: Boolean = true, maxRedirects: Int = 5)
                     (redirect: (Request[F], Response[F]) => Option[Request[F]] = redirectByLocation[F])
                     (observe: Response[F] => Pipe[F, Byte, Nothing] = (response: Response[F]) => (in: Stream[F, Byte]) => in.drain)
                     (using Client[F])(using Async[F], Files[F])
   : EitherT[F, Error, Unit] =
-    for
-      path <- path(request, directory, fileName).toRight(OptionEmpty.label("fileName")).eLiftET[F]
-      _ <- path.createParentDirectories[F]().asET
-      _ <- bodyWithRedirects[F](request, maxRedirects)(redirect)(observe).through(Files[F].writeAll(path))
-        .compile.drain.asET
-    yield
-      ()
-
-  def downloadIfNotExists[F[_]](request: Request[F], directory: Option[Path] = None, fileName: Option[Path] = None, maxRedirects: Int = 5)
-                               (redirect: (Request[F], Response[F]) => Option[Request[F]] = redirectByLocation[F])
-                               (observe: Response[F] => Pipe[F, Byte, Nothing] = (response: Response[F]) => (in: Stream[F, Byte]) => in.drain)
-                               (using Client[F])(using Async[F], Files[F])
-  : EitherT[F, Error, Unit] =
     type G[X] = EitherT[F, Error, X]
     for
       path <- path(request, directory, fileName).toRight(OptionEmpty.label("fileName")).eLiftET[F]
-      _ <- Monad[G].ifM[Unit](Files[F].exists(path).asET)(
+      _ <- Monad[G].ifM[Unit](if overwrite then false.pure[G] else Files[F].exists(path).asET)(
         ().rLiftET,
-        path.createParentDirectories[F]().asET
-          .flatMap(_ => bodyWithRedirects[F](request, maxRedirects)(redirect)(observe).through(Files[F].writeAll(path))
+        for
+          _ <- path.createParentDirectories[F]().asET
+          part = Path(s"$path.part")
+          _ <- bodyWithRedirects[F](request, maxRedirects)(redirect)(observe).through(Files[F].writeAll(part))
             .compile.drain.asET
-          )
+          _ <- Files[F].move(part, path).asET
+        yield
+          ()
       )
     yield
       ()
--- http-client/jvm/src/test/scala/com/peknight/http/client/DownloadFlatSpec.scala
+++ http-client/jvm/src/test/scala/com/peknight/http/client/DownloadFlatSpec.scala
@@ -3,7 +3,7 @@ package com.peknight.http.client
 import cats.effect.IO
 import cats.effect.testing.scalatest.AsyncIOSpec
 import cats.syntax.option.*
-import com.peknight.http.client.download.downloadIfNotExists
+import com.peknight.http.client.download.download
 import fs2.io.file.Path
 import org.http4s.Method.GET
 import org.http4s.Uri
@@ -16,7 +16,7 @@ class DownloadFlatSpec extends AsyncFlatSpec with AsyncIOSpec:
   "Download" should "pass" in {
     val uri: Uri = uri"https://nexus.peknight.com/repository/maven-public/org/typelevel/cats-core_3/2.13.0/cats-core_3-2.13.0.pom"
     EmberClientBuilder.default[IO].withMaxResponseHeaderSize(16384).build
-      .use(client => downloadIfNotExists[IO](GET(uri), Path("test").some)()(showProgressInConsole[IO])(using client).value)
+      .use(client => download[IO](GET(uri), Path("test").some)()(showProgressInConsole[IO])(using client).value)
       .map(either => assert(either.isRight))
   }
 end DownloadFlatSpec
